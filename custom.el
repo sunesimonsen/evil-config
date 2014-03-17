63f89ef069151bf5d12e3c1b6d37b4d51bd3f4ba
Sune Simonsen <sune@we-knowhow.dk>
2014-03-17 13:22:53 +0100

    Initial commit

---
+++ custom.el
@@ -0,0 +1,40 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote ((".*" . "~/.evil.backups"))))
+ '(blink-cursor-mode nil)
+ '(case-replace nil)
+ '(dabbrev-case-distinction nil)
+ '(dabbrev-case-replace nil)
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(global-auto-revert-mode t)
+ '(goto-alternate-git-file-patterns (quote (("\\.spec\\.js$" . ".js") ("\\.h$" . ".c") ("Test\\.java$" . ".java") ("\\.ko$" . ".less") ("\\.less$" . ".ko"))))
+ '(hl-paren-colors (quote ("green" "yellow" "magenta" "orange")))
+ '(ido-auto-merge-delay-time 100)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message "")
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 1000)
+ '(scroll-margin 4)
+ '(scroll-step 1)
+ '(scss-compile-at-save nil)
+ '(sgml-basic-offset 4)
+ '(surround-pairs-alist (quote ((40 "(" . ")") (91 "[" . "]") (123 "{" . "}") (41 "(" . ")") (93 "[" . "]") (125 "{" . "}") (35 "#{" . "}") (98 "(" . ")") (66 "{" . "}") (62 "<" . ">") (116 . surround-read-tag) (60 . surround-read-tag))))
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(truncate-lines t)
+ '(visible-bell t)
+ '(x-select-enable-clipboard t)
+ '(yas-prompt-functions (quote (yas-ido-prompt yas-x-prompt yas-dropdown-prompt yas-completing-prompt yas-ido-prompt yas-no-prompt)))
+ '(yas/prompt-functions (quote (yas/ido-prompt yas/x-prompt yas/dropdown-prompt yas/completing-prompt yas/no-prompt))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(error ((t (:underline t))))
+ '(lazy-highlight ((t (:background "gray13")))))
---
+++ emacs.el
@@ -0,0 +1,265 @@
+(require 'thingatpt)
+
+;;; Setup the path
+(let ((path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
+
+;;; Customize
+(setq custom-file "~/bin/dotfiles/evil.d/custom.el")
+(load custom-file)
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(setq el-get-user-package-directory "~/.emacs.d/el-get-init-files/")
+
+;;; IDo mode
+(ido-mode 't)
+(setq ido-enable-flex-matching 't)
+(define-key ido-file-dir-completion-map (kbd "C-c C-s")
+      (lambda()
+        (interactive)
+        (ido-initiate-auto-merge (current-buffer))))
+
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
+
+; Font
+(set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
+
+; stop forcing me to spell out "yes"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(define-key key-translation-map (kbd "½") (kbd "$"))
+
+(require 'uniquify)
+(set-variable 'uniquify-buffer-name-style 'forward)
+
+;;; auto-mode-alist
+(add-to-list 'auto-mode-alist '("\\.ko$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.cjson$" . js-mode))
+
+(setq
+ el-get-sources
+ '((:name el-get)
+   (:name evil-surround)
+   (:name evil-numbers)
+   (:name flymake-cursor)
+   (:name markdown-mode)
+   (:name ace-jump-mode)
+   (:name evil-nerd-commenter)
+   (:name evil-little-word)
+   (:name ido-goto-symbol)
+   (:name ido-bookmark-jump)
+   (:name todotxt-mode)
+   (:name find-file-in-git-repo)
+   (:name evil-walk-on-the-edge)
+   (:name magit)
+   (:name less-css-mode)
+
+   (:name color-theme
+	  :after
+	  (progn
+	    (color-theme-initialize)
+	    (defun light-theme ()
+	      (interactive)
+	      (color-theme-standard))
+
+	    (defun dark-theme ()
+	      (interactive)
+	      (color-theme-billw))
+
+	    (dark-theme)))
+
+
+   (:name evil
+	  :after
+	  (progn
+        (setq evil-search-module 'evil-search)
+	    (evil-mode 't)
+
+	    (define-key evil-motion-state-map "æ" 'evil-ex)
+	    (define-key evil-normal-state-map "Æ" 'evil-execute-macro)
+
+	    (define-key evil-motion-state-map "å" 'evil-move-backward-paren)
+	    (define-key evil-motion-state-map "ø" 'evil-move-forward-paren)
+
+	    (define-key evil-motion-state-map "Å" 'evil-backward-paragraph)
+	    (define-key evil-motion-state-map "Ø" 'evil-forward-paragraph)
+
+	    (define-key evil-motion-state-map (kbd "SPC") 'evil-search-forward)
+	    (define-key evil-motion-state-map (kbd "C-SPC") 'evil-search-backward)
+
+	    (define-key evil-visual-state-map (kbd "C-æ") 'evil-exit-visual-state)
+	    (define-key evil-insert-state-map (kbd "C-æ") 'evil-normal-state)
+	    (define-key evil-replace-state-map (kbd "C-æ") 'evil-normal-state)
+	    (define-key evil-normal-state-map (kbd "C-æ") 'evil-force-normal-state)
+
+	    (define-key evil-outer-text-objects-map "å" 'evil-a-bracket)
+	    (define-key evil-outer-text-objects-map "ø" 'evil-a-bracket)
+	    (define-key evil-outer-text-objects-map "Å" 'evil-a-curly)
+	    (define-key evil-outer-text-objects-map "Ø" 'evil-a-curly)
+
+	    (define-key evil-motion-state-map (kbd "M-h") 'evil-window-left)
+	    (define-key evil-motion-state-map (kbd "M-j") 'evil-window-down)
+	    (define-key evil-motion-state-map (kbd "M-k") 'evil-window-up)
+	    (define-key evil-motion-state-map (kbd "M-l") 'evil-window-right)
+	    (define-key evil-motion-state-map (kbd "M-n") 'evil-window-new)
+	    (define-key evil-motion-state-map (kbd "M-o") 'delete-other-windows)
+	    (define-key evil-motion-state-map (kbd "M-p") 'evil-window-mru)
+	    (define-key evil-motion-state-map (kbd "M-c") 'delete-window)
+	    (define-key evil-motion-state-map (kbd "M-v") 'split-window-horizontally)
+	    (define-key evil-motion-state-map (kbd "M-s") 'split-window-vertically)
+	    (define-key evil-motion-state-map (kbd "M--") 'evil-window-decrease-height)
+	    (define-key evil-motion-state-map (kbd "M-+") 'evil-window-increase-height)
+
+
+	    (define-key evil-inner-text-objects-map "å" 'evil-inner-bracket)
+	    (define-key evil-inner-text-objects-map "ø" 'evil-inner-bracket)
+	    (define-key evil-inner-text-objects-map "Å" 'evil-inner-curly)
+	    (define-key evil-inner-text-objects-map "Ø" 'evil-inner-curly)
+
+	    (define-key evil-motion-state-map "gt" 'evil-jump-to-tag)
+	    (define-key evil-motion-state-map "gk" 'evil-ace-jump-word-mode)
+	    (define-key evil-motion-state-map "gå" 'flymake-goto-prev-error)
+	    (define-key evil-motion-state-map "gø" 'flymake-goto-next-error)
+
+	    (define-key evil-motion-state-map "\C-ø" 'evil-jump-to-tag)
+
+	    (define-key evil-visual-state-map (kbd "<tab>") ">gv")
+	    (define-key evil-visual-state-map (kbd "<backtab>") "<gv")
+
+	    (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
+	    (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
+
+	    (define-prefix-command 'evil-leader-map)
+	    (define-key evil-leader-map "," 'evil-repeat-find-char)
+	    (define-key evil-leader-map "p" 'find-file-in-git-repo)
+	    (define-key evil-leader-map "o" 'ido-goto-symbol)
+	    (define-key evil-leader-map "e" 'ido-find-file)
+	    (define-key evil-leader-map "b" 'ido-switch-buffer)
+	    (define-key evil-leader-map "w" 'evil-write)
+	    (define-key evil-leader-map "a" 'goto-alternate-git-file)
+	    (define-key evil-leader-map "g" 'ido-bookmark-jump)
+	    (define-key evil-motion-state-map "," 'evil-leader-map)
+
+	    (define-key evil-motion-state-map ";" 'evil-repeat-find-char-reverse)
+
+	    (define-key evil-normal-state-map "g," 'goto-last-change)
+	    (define-key evil-normal-state-map "g;" 'goto-last-change-reverse)
+
+	    (defun query-replace-symbol-at-point (replacement)
+	      "Query replace the symbol at point with the given replacement"
+	      (interactive "swith: ")
+	      (beginning-of-thing 'symbol)
+	      (let* ((symbol-at-point (thing-at-point 'symbol))
+                 (pattern (format "\\_<%s\\_>" (regexp-quote symbol-at-point))))
+            (query-replace-regexp pattern replacement)))
+
+	    (define-key evil-normal-state-map "gs" 'query-replace-symbol-at-point)
+
+	    (define-key minibuffer-local-map [escape] 'keyboard-escape-quit)
+	    (define-key minibuffer-local-ns-map [escape] 'keyboard-escape-quit)
+	    (define-key minibuffer-local-completion-map [escape]
+	      'keyboard-escape-quit)
+	    (define-key minibuffer-local-must-match-map [escape]
+	      'keyboard-escape-quit)
+	    (define-key minibuffer-local-isearch-map [escape]
+	      'keyboard-escape-quit)
+
+	    (evil-ex-define-cmd "bo[okmarks]" 'ido-bookmark-jump)
+	    (evil-ex-define-cmd "p[roject]" 'ido-project-root-find-file)
+	    (evil-ex-define-cmd "b[uffer]" 'ido-switch-buffer)
+	    (evil-ex-define-cmd "e[dit]" 'ido-find-file)
+
+        (evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
+          "K" 'magit-discard-item
+          "L" 'magit-key-mode-popup-logging)
+
+        (evil-add-hjkl-bindings magit-status-mode-map 'emacs
+          "K" 'magit-discard-item
+          "L" 'magit-key-mode-popup-logging
+          "H" 'magit-toggle-diff-refine-hunk)
+
+        (evil-add-hjkl-bindings bookmark-bmenu-mode-map 'emacs
+          "K" 'bookmark-bmenu-delete
+          "L" 'bookmark-bmenu-load
+          )
+
+        (evil-add-hjkl-bindings occur-mode-map 'emacs)
+
+        (evil-add-hjkl-bindings magit-log-mode-map 'emacs
+          "L" 'magit-key-mode-popup-logging)
+        ))
+
+   (:name smex
+	  :after
+	  (progn
+	    ;; SMEX
+	    (setq smex-save-file "~/.emacs.d/.smex-items")
+
+	    (defun lazy-load-smex ()
+	      (interactive)
+	      (or (boundp 'smex-cache)
+		  (smex-initialize))
+	      (global-set-key [(meta x)] 'smex)
+	      (global-set-key [menu] 'smex)
+	      (smex))
+
+        (global-set-key [menu] 'lazy-load-smex)
+        (global-set-key [(meta x)] 'lazy-load-smex)))
+
+   (:name yasnippet
+      :after
+      (progn
+        (setq yas-snippet-dirs
+              '("~/bin/dotfiles/evil.d/snippets"))
+        (yas-global-mode 't)))
+
+   (:name flymake-node-jshint
+          :after
+          (progn
+            (add-hook 'js-mode-hook (lambda () (flymake-mode 1)))))
+ ))
+
+(setq my:el-get-packages
+       (loop for src in el-get-sources collect (el-get-source-name src)))
+
+(add-to-list 'el-get-recipe-path "~/bin/dotfiles/evil.d/recipes")
+(el-get 'sync my:el-get-packages)
+
+(add-hook 'ido-setup-hook '(lambda ()
+        (define-key ido-completion-map (kbd "<C-return>") 'ido-select-text)
+        (define-key ido-completion-map (kbd "C-j") 'ido-next-match)
+        (define-key ido-completion-map (kbd "C-k") 'ido-prev-match)
+        (define-key ido-completion-map (kbd "<C-delete>") 'ido-delete-file-at-head)
+        ))
+
+;;; IMenu
+(defun my-js-imenu-make-index ()
+  (save-excursion
+    (imenu--generic-function '((nil "function\\s-+\\([^ ]+\\)(" 1)
+                               (nil "\\.\\([^\\. ]+\\)\\s-*=\\s-*function\\s-*(" 1)))))
+
+(defun my-js-mode-hook ()
+  (setq imenu-create-index-function 'my-js-imenu-make-index))
+
+(add-hook 'js-mode-hook 'my-js-mode-hook)
+
+;;; TodoTxt
+(setq todotxt-default-file (expand-file-name "~/Dropbox/todo/todo.txt"))
+(add-to-list 'auto-mode-alist '("/todo.txt$" . todotxt-mode))
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
